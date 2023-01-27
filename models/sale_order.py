e41049e3bb56cde8f95484ba7b668e9149c41ab7
jwaes <jaco.waes@gmail.com>
2023-01-27 13:57:01 +0000

    default housing project

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,10 +4,15 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    def _default_housing_project_id(self):
+        if  self.housing_batch_id and not self.housing_project_id:
+            self.housing_project_id = self.housing_batch_id.housing_project_id
+
     housing_batch_id = fields.Many2one(
         'jt.housing.batch', string='Housing batch', copy=False, tracking=True)
 
-    housing_project_id = fields.Many2one('jt.housing.project', string='Housing Project', copy=False, tracking=True)
+    housing_project_id = fields.Many2one('jt.housing.project', string='Housing Project', copy=False, tracking=True, default=_default_housing_project_id)
+
 
     def go_to_housing_batch(self):
         self.ensure_one()
